dcb8fabd68d2222370ede19c402e389bfcfe3638
coreyfedde <coreysfedde@gmail.com>
2017-08-26 16:18:41 -0400

    Adds button and AJAX for crud
    - Created button to test crud actions
    - Created AJAX commands
    - Created ui success and failure messages for AJAX

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,17 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const user = require('./auth/user.js')
+const ui = require('./auth/auth-ui.js')
 
 $(() => {
   setAPIOrigin(location, config)
+  $('#signUpForm').on('submit', user.onSignUp)
+  $('#signInForm').on('submit', user.onLogIn)
+  $('#changePasswordForm').on('submit', user.onChangePassword)
+  $('#logOutButton').on('click', user.onLogOut)
+  $('#changePasswordButton').hide()
+  $('#logOutButton').hide()
 })
 
 // use require with a reference to bundle the file and use it in this file
