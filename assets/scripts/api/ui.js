c90bd2b9edaf6b107bd9de80c87bee226444370e
coreyfedde <coreysfedde@gmail.com>
2017-09-09 12:19:23 -0400

    Creates template for loan selector
    - Creates handlebars template for loan selector
    - Adds loan selector to creation to success of get request

--- assets/scripts/api/ui.js
+++ assets/scripts/api/ui.js
@@ -1,12 +1,15 @@
 'use strict'
 const store = require('../store')
 const loansTemplate = require('../templates/show-loans.handlebars')
+const loanSelectTemplate = require('../templates/loan-select.handlebars')
 
 const onGetLoansSuccess = function (data) {
   // console.log("onGetLoansSuccess data:", data)
+  const loanSelectHtml = loanSelectTemplate({ loans: data.loans })
   const loansHtml = loansTemplate({ loans: data.loans })
   $('#board').append(loansHtml)
-  $('#board').show()
+  // $('#board').show()
+  $('#reviewBoard').prepend(loanSelectHtml)
   $('.loan-info').hide()
 }
 
