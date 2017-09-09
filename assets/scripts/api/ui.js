213e2699ae819d3a2da2224ec1dbb5bef0761584
coreyfedde <coreysfedde@gmail.com>
2017-09-09 18:58:50 -0400

    Adds dummy pie chart to loan review
    - Added function to create array of data for pie chart
    - Added graph button to create pie chart
    Next: need to turn getPieData into a promise so I can chain the graph
    function to it in a .then()

--- assets/scripts/api/ui.js
+++ assets/scripts/api/ui.js
@@ -3,6 +3,7 @@ const store = require('../store')
 const loansTemplate = require('../templates/show-loans.handlebars')
 const loanTemplate = require('../templates/show-loan.handlebars')
 const loanSelectTemplate = require('../templates/loan-select.handlebars')
+const chart = require('../d3-charts.js')
 
 const onGetLoansSuccess = function (data) {
   // console.log("onGetLoansSuccess data:", data)
@@ -16,15 +17,27 @@ const onGetLoansSuccess = function (data) {
   $('.loan-info').hide()
 }
 
+let pieData = []
+
 const onGetLoanSuccess = function (data) {
   const loanHtml = loanTemplate({ loan: data.loan })
   $('#reviewBoard').append(loanHtml)
   $('#interestBoard').append(loanHtml)
   $('#paymentBoard').append(loanHtml)
   $('.loan-info').hide()
+  getPieData(data)
+}
+
+const getPieData = function (data) {
+  let dataPoint = data.loan.principal
+  let secondDataPoint = data.loan.total_interest
+  pieData.push(dataPoint)
+  pieData.push(secondDataPoint)
+  console.log(pieData)
 }
 
 module.exports = {
   onGetLoansSuccess,
-  onGetLoanSuccess
+  onGetLoanSuccess,
+  pieData
 }
