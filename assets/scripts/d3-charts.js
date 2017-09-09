19112d85fae5f1bb0552346f4b82944ca39ffc53
coreyfedde <coreysfedde@gmail.com>
2017-09-09 11:55:46 -0400

    Displays sample visualizations
    - Changed names for different view boards and visualization sections
    - Set sample visualization to display on correct boards

--- assets/scripts/d3-charts.js
+++ assets/scripts/d3-charts.js
@@ -1,11 +1,11 @@
 const d3 = require("d3");
 const api = require('./api/api.js')
 
-// const dataArray = api.dataArray
-let dataArray = [0, 5, 1, 3, 2, 6, 7]
-let graph = function (event) {
+const dataArray = api.dataArray
+// let dataArray = [0, 5, 1, 3, 2, 6, 7]
+let barChart = function (event) {
     console.log(dataArray)
-    let svg = d3.select("body").append("svg")
+    let svg = d3.select("#visualizations").append("svg")
               .attr("height","100%")
               .attr("width","100%");
 
@@ -16,7 +16,7 @@ let graph = function (event) {
               .attr("height", function(d, i) {return (d * 10)})
               .attr("width","40")
               .attr("x", function(d, i) {return (i * 60) + 25})
-              .attr("y", function(d, i) {return 400 - (d * 10)})
+              .attr("y", function(d, i) {return 200 - (d * 10)})
 
     svg.selectAll("text")
         .data(dataArray)
@@ -24,9 +24,9 @@ let graph = function (event) {
         .text(function(d) {return d})
                .attr("class", "text")
                .attr("x", function(d, i) {return (i * 60) + 36})
-               .attr("y", function(d, i) {return 415 - (d * 10)})
+               .attr("y", function(d, i) {return 215 - (d * 10)})
 }
 
 module.exports = {
-  graph
+  barChart
 }
